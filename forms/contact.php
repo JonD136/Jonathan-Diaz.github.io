20ed08dd10665d6e7c502888ca7af1f5fb2f2fbe
Jonathan Diaz <jdiaz1697@gmail.com>
2022-12-29 14:53:48 -0500

    contact me

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
+  /*
   $contact->smtp = array(
-    'host' => 'jonathanjdiaz.com',
-    'username' => 'jdiaz1697@gmail.com',
-    'password' => 'Joel1997!',
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
     'port' => '587'
   );
-  
+  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
